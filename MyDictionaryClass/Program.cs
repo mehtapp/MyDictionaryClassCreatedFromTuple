4e226483f3173c9819e7e7edb906a661c97c4166
Mehtap Demir <mehtap.demir011@gmail.com>
2024-07-09 20:35:13 +0300

    Over

--- MyDictionaryClass/Program.cs
+++ MyDictionaryClass/Program.cs
@@ -1,35 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
+﻿
 MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
-
-
-
-Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+//Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
 Console.WriteLine("Başlangıç count : " + myDictionary.Count);
-
 myDictionary.Add(1, "Mehtap");
 myDictionary.Add(1000, "Kemal");
-
 Console.WriteLine("Değer eklendi : " +myDictionary.Count);
 Console.WriteLine("GÜncel Veriler");
 foreach (var dictionary in myDictionary.GetDictionary())
 {
     Console.WriteLine(dictionary.Item1 + " " + dictionary.Item2);
 }
-
 myDictionary.Clear();
-
 Console.WriteLine("Liste boşaltıldı. " + myDictionary.Count);
 Console.WriteLine("Teyit içindeki veriler");
 foreach (var dictionary in myDictionary.GetDictionary())
 {
     Console.WriteLine(dictionary.Item1 + " " + dictionary.Item2);
 }
-//keyValuePairs.Count;
-
-
 
 
 public class MyDictionary<K, V>  // K key için kullanılacak generic type'ı , K key için kullanılacak generic type'ı temsil eder.
@@ -44,7 +31,6 @@ public class MyDictionary<K, V>  // K key için kullanılacak generic type'ı , 
     public int Count
     {
         get { return _count; }
-
     }
     public void Add(K key, V value)
     {
